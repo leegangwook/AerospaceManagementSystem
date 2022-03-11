4b135fc570b3205224e5bdea4def7a7dd2963fd4
leegangwook <rkddnr4618@naver.com>
2022-03-11 21:47:18 +0900

    removed menumanager.java and added menumanager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -2,7 +2,7 @@
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("¡ÚAerospaceManagementSystem-aircrafts¡Ú");
+		System.out.println("*AerospaceManagementSystem-Aircrafts*");
 		System.out.println("1. Add Aircrafts");
 		System.out.println("2. Delete Aircrafts");
 		System.out.println("3. Edit Aircrafts");
