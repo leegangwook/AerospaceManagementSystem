bf4044f73c881a274551233bc67a4506b17c8509
leegangwook <rkddnr4618@naver.com>
2022-03-11 19:34:12 +0900

    removed hello.java and added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,13 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Aircrafts");
+		System.out.println("2. Delete Aircrafts");
+		System.out.println("3. Edit Aircrafts");
+		System.out.println("4. View Aircrafts");
+		System.out.println("5. Exit");
+
+	}
+
+}
