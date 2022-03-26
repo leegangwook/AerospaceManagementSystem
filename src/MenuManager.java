bb9676a9b61108c8b2175b45ed7ee462871f3cd2
leegangwook <rkddnr4618@naver.com>
2022-03-26 20:38:06 +0900

    add method selection

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -14,27 +14,49 @@ public class MenuManager {
 			System.out.println("4. View Aircrafts");
 			System.out.println("5. Show a menu");
 			System.out.println("6. Exit");
-			System.out.println("Select one number between 1-6");
+			System.out.print("Select one number between 1-6:");
 			num = input.nextInt();
 			
 			switch(num) {
 				case 1:
-					System.out.print("Aircraft Name:");
-					String AircraftName1 = input.next();
-					System.out.print("Aircraft Type:");
-					String AircraftType = input.next();
-					System.out.print("Aircraft Manufacturer:");
-					String AircraftManufacturer = input.next();
-					System.out.print("Aircraft Cost:");
-					String AircraftCost = input.next();
+					AddAircrafts();
 					break;
 				case 2:
+					DeleteAircrafts();
+					break;
 				case 3:
+					EditAircrafts();
+					break;
 				case 4:
-					System.out.print("Aircraft Name:");
-					String AircraftName2 = input.next();	
+					ViewAircrafts();
+					break;
 			}
 		}
 	}
-
+	public static void AddAircrafts() {
+		Scanner input = new Scanner(System.in);	
+		System.out.print("Aircraft Name:");
+		String AircraftName1 = input.next();
+		System.out.print("Aircraft Type:");
+		String AircraftType = input.next();
+		System.out.print("Aircraft Manufacturer:");
+		String AircraftManufacturer = input.next();
+		System.out.print("Aircraft Cost:");
+		String AircraftCost = input.next(); 
+	}
+	public static void DeleteAircrafts() {
+		Scanner input = new Scanner(System.in);	
+		System.out.print("Aircraft Name:");
+		String AircraftName1 = input.next();
+	}
+	public static void EditAircrafts() {
+		Scanner input = new Scanner(System.in);	
+		System.out.print("Aircraft Name:");
+		String AircraftName1 = input.next();
+	}
+	public static void ViewAircrafts() {
+		Scanner input = new Scanner(System.in);	
+		System.out.print("Aircraft Name:");
+		String AircraftName1 = input.next();
+	}
 }
